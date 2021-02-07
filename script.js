1589e82368b1e37331131a61c690f0cafb0019b5
PauloBacelar <paulobacelara@gmail.com>
2021-02-07 18:25:20 -0300

    Added range in HTML

---
+++ script.js
@@ -0,0 +1,20 @@
+// Global variables
+const downPayment = document.getElementById("down-payment");
+const homePrice = document.querySelector("input#house-value");
+const interestRate = document.querySelector("input#interest-rate");
+const monthly = document.querySelector("input#monthly");
+const yearsResult = document.getElementById("years-result");
+const years = document.getElementById("years");
+const button = document.querySelector("button#calc-btn");
+
+// Main functions
+button.addEventListener("click", function() {
+    if(!checkInput()){
+
+    }
+});
+
+// Functions
+function checkInput() {
+    
+}
